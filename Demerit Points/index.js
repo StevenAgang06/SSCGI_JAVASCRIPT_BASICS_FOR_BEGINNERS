d364fe790626105ba0c0fa99649a13b92a571ce5
StevenAgang06 <suagang@sscgi.com>
2025-02-28 14:11:23 +0800

    add demerit points exercise

---
+++ Demerit Points/index.js
@@ -0,0 +1,16 @@
+// Speed Limit 70km/h
+// 5 -> 1 point
+// Math.floor(1.3)
+// 12 points -> suspended
+function checkSpeed(speed) {
+  if (checkSpeed < 70) return "OK";
+
+  let points = (speed - 70) / 5;
+  points = Math.floor(points);
+  if (points > 0 && points < 12) return points;
+  if (points > 11) return "License Suspended";
+
+  return "OK";
+}
+
+console.log(checkSpeed(80));
